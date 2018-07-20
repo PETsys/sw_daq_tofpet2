78fac4c0b28abc19d064a3d68ef4b4b11c7ed491
Ricardo Bugalho <ricardo@lip.pt>
2018-07-20 18:30:57 +0100

    Connection::acquire()   Dump data frames during 50 ms to allow DAQ card to recover from pause after sending last command

--- src/petsys_py_lib/daqd.py
+++ src/petsys_py_lib/daqd.py
@@ -1016,6 +1016,13 @@ class Connection:
 		wrPointer, rdPointer = (0, 0)
 		while wrPointer == rdPointer:
 			wrPointer, rdPointer = self.__getDataFrameWriteReadPointer()
+
+		t0 = time()
+		# For 50 ms, dump all data
+		while (time() - t0) < 0.050:
+			# Set the read pointer to write pointer, in order to consume all available frames in buffer
+			wrPointer, rdPointer = self.__getDataFrameWriteReadPointer();
+			self.__setDataFrameReadPointer(wrPointer);
 		
 		bs = self.__shm.getSizeInFrames()
 		index = rdPointer % bs
