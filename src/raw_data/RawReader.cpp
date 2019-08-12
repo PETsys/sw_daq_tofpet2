ea14ad985f5cbcbc3291a1948b0b7f24b0086723
ferras <ferras@tagus22.tagus.lip.pt>
2019-08-12 14:27:22 +0100

    BUGFIX: Corrected default trigger ID assigmenent when it not present in raw file HEADER

--- src/raw_data/RawReader.cpp
+++ src/raw_data/RawReader.cpp
@@ -63,7 +63,12 @@ RawReader *RawReader::openFile(const char *fnPrefix)
 	reader->dataFile = rawFile;
 	reader->frequency = header[0] & 0xFFFFFFFFUL;
 	reader->qdcMode = (header[0] & 0x100000000UL) != 0;
-	if (header[2] & 0x8000 != 0) { reader->triggerID = header[2] & 0x7FFF; }
+	if (header[2] & 0x8000 != 0) { 
+		reader->triggerID = header[2] & 0x7FFF; 
+	}
+	else {
+		reader->triggerID = -1;
+	}
 
 	Step step;
 	while(fscanf(idxFile, "%lu\t%lu\t%lld\t%lld\t%f\t%f", &step.stepBegin, &step.stepEnd, &step.stepFirstFrame, &step.stepLastFrame, &step.step1, &step.step2) == 6) {
